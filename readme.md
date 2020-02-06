dd0b71c14f4c1e21b44053839ab8a5d46880c909
Chandan Singh <chandan_singh@berkeley.edu>
2020-02-06 10:24:11 -0800

    add jointplot + pc_plot

--- readme.md
+++ readme.md
@@ -11,3 +11,14 @@ Generating beautiful and customizable heatmaps, trying to replicate some of the 
 - simple example:
 
 ![](superheat.png)
+
+## grouped jointplot
+
+Based on answer from [here](https://stackoverflow.com/questions/35920885/how-to-overlay-a-seaborn-jointplot-with-a-marginal-distribution-histogram-fr).
+
+![](jointplot_grouped.png)
+
+## pc plot
+
+![](pc_plot.png)
+
--- viz.py
+++ viz.py
@@ -150,3 +150,66 @@ def plot_pcs(pca, X):
     colorAx = plt.subplot(gs[3])
     cb = plt.colorbar(p, cax=colorAx)
     plt.show()
+    
+def jointplot_grouped(col_x: str, col_y: str, col_k: str, df, 
+                      k_is_color=False, scatter_alpha=.5, add_global_hists: bool=True):
+    '''Jointplot of hists + densities
+    Params
+    ------
+    col_x
+        name of X var
+    col_y
+        name of Y var
+    col_k
+        name of variable to group/color by
+    add_global_hists
+        whether to plot the global hist as well
+    '''
+    def colored_scatter(x, y, c=None):
+        def scatter(*args, **kwargs):
+            args = (x, y)
+            if c is not None:
+                kwargs['c'] = c
+            kwargs['alpha'] = scatter_alpha
+            plt.scatter(*args, **kwargs)
+
+        return scatter
+
+    g = sns.JointGrid(
+        x=col_x,
+        y=col_y,
+        data=df
+    )
+    color = None
+    legends=[]
+    for name, df_group in df.groupby(col_k):
+        legends.append(name)
+        if k_is_color:
+            color=name
+        g.plot_joint(
+            colored_scatter(df_group[col_x],df_group[col_y],color),
+        )
+        sns.distplot(
+            df_group[col_x].values,
+            ax=g.ax_marg_x,
+            color=color,
+        )
+        sns.distplot(
+            df_group[col_y].values,
+            ax=g.ax_marg_y,
+            color=color,            
+            vertical=True
+        )
+    if add_global_hists:
+        sns.distplot(
+            df[col_x].values,
+            ax=g.ax_marg_x,
+            color='grey'
+        )
+        sns.distplot(
+            df[col_y].values.ravel(),
+            ax=g.ax_marg_y,
+            color='grey',
+            vertical=True
+        )
+    plt.legend(legends)
